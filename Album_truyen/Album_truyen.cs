a0d622a69c4e72aba0166c49d69dca0979a7be2c
minhhien195 <huynhhien225@gmail.com>
2024-04-18 19:42:42 +0700

    project

---
+++ Album_truyen/Album_truyen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Album_truyen
+{
+    public partial class Album_truyen : Form
+    {
+        public Album_truyen()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Readinghistory/Readinghistory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+
+namespace Readinghistory
+{
+    public partial class Readinghistory : Form
+    {
+/*        const Config = new FirebaseConfig
+        {
+            AuthKey = "38QvLmnKMHlQtJ9yZzCqqWytxeXimwt06ZnFfSc2",
+            RealtimeDatabaseURL = "https://healtruyen-default-rtdb.asia-southeast1.firebasedatabase.app/",
+        };*/
+        IFirebaseClient Client;
+        public Readinghistory()
+        {
+            InitializeComponent();
+        }
+
+        private void Readinghistory_Load(object sender, EventArgs e)
+        {
+           // Client = new FireSharp.FirebaseClient(Config);
+
+            if (Client != null)
+            {
+                MessageBox.Show("Connected successfully!");
+            }
+        }
+    }
+}
