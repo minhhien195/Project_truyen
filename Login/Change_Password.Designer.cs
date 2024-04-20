e7d827a146380cf1cef9c2f173d4598b346ceb08
minhhien195 <huynhhien225@gmail.com>
2024-04-20 23:15:30 +0700

    login

--- Login/Change_Password.Designer.cs
+++ Login/Change_Password.Designer.cs
@@ -42,18 +42,23 @@
             this.txtMK = new System.Windows.Forms.TextBox();
             this.btnXacnhan = new System.Windows.Forms.Button();
             this.Changepwd_label = new System.Windows.Forms.Label();
+            this.ptrNotsame = new System.Windows.Forms.PictureBox();
+            this.lbNotsame = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ptrHide1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.prtHide)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptrView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptrView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrNotsame)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.AutoSize = true;
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(227)))), ((int)(((byte)(243)))));
+            this.panel1.Controls.Add(this.ptrNotsame);
+            this.panel1.Controls.Add(this.lbNotsame);
             this.panel1.Controls.Add(this.ptrHide1);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.lbChangpwd);
@@ -68,10 +73,10 @@
             this.panel1.Controls.Add(this.Changepwd_label);
             this.panel1.Font = new System.Drawing.Font("League Spartan", 12F);
             this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.panel1.Location = new System.Drawing.Point(567, 164);
+            this.panel1.Location = new System.Drawing.Point(638, 205);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(554, 371);
+            this.panel1.Size = new System.Drawing.Size(623, 464);
             this.panel1.TabIndex = 1;
             // 
             // ptrHide1
@@ -80,10 +85,10 @@
             this.ptrHide1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ptrHide1.Image = global::Login.Properties.Resources.hide;
             this.ptrHide1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.ptrHide1.Location = new System.Drawing.Point(475, 258);
+            this.ptrHide1.Location = new System.Drawing.Point(534, 322);
             this.ptrHide1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ptrHide1.Name = "ptrHide1";
-            this.ptrHide1.Size = new System.Drawing.Size(36, 30);
+            this.ptrHide1.Size = new System.Drawing.Size(40, 38);
             this.ptrHide1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ptrHide1.TabIndex = 22;
             this.ptrHide1.TabStop = false;
@@ -93,10 +98,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(225)))));
             this.pictureBox1.Image = global::Login.Properties.Resources.exclamation_mark;
-            this.pictureBox1.Location = new System.Drawing.Point(128, 85);
+            this.pictureBox1.Location = new System.Drawing.Point(136, 106);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(39, 37);
+            this.pictureBox1.Size = new System.Drawing.Size(44, 46);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 21;
             this.pictureBox1.TabStop = false;
@@ -108,10 +113,10 @@
             this.lbChangpwd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lbChangpwd.Font = new System.Drawing.Font("League Spartan SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbChangpwd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(0)))));
-            this.lbChangpwd.Location = new System.Drawing.Point(124, 80);
+            this.lbChangpwd.Location = new System.Drawing.Point(132, 100);
             this.lbChangpwd.Name = "lbChangpwd";
-            this.lbChangpwd.Padding = new System.Windows.Forms.Padding(36, 8, 9, 8);
-            this.lbChangpwd.Size = new System.Drawing.Size(333, 48);
+            this.lbChangpwd.Padding = new System.Windows.Forms.Padding(40, 10, 10, 10);
+            this.lbChangpwd.Size = new System.Drawing.Size(391, 58);
             this.lbChangpwd.TabIndex = 20;
             this.lbChangpwd.Text = "Bạn cần phải thay đổi mật khẩu";
             // 
@@ -121,10 +126,10 @@
             this.prtHide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.prtHide.Image = global::Login.Properties.Resources.hide;
             this.prtHide.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.prtHide.Location = new System.Drawing.Point(475, 189);
+            this.prtHide.Location = new System.Drawing.Point(534, 236);
             this.prtHide.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.prtHide.Name = "prtHide";
-            this.prtHide.Size = new System.Drawing.Size(36, 30);
+            this.prtHide.Size = new System.Drawing.Size(40, 38);
             this.prtHide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.prtHide.TabIndex = 17;
             this.prtHide.TabStop = false;
@@ -136,10 +141,10 @@
             this.ptrView.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ptrView.Image = global::Login.Properties.Resources.view1;
             this.ptrView.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.ptrView.Location = new System.Drawing.Point(474, 189);
+            this.ptrView.Location = new System.Drawing.Point(533, 236);
             this.ptrView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ptrView.Name = "ptrView";
-            this.ptrView.Size = new System.Drawing.Size(36, 30);
+            this.ptrView.Size = new System.Drawing.Size(40, 38);
             this.ptrView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ptrView.TabIndex = 16;
             this.ptrView.TabStop = false;
@@ -151,10 +156,10 @@
             this.ptrView1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ptrView1.Image = global::Login.Properties.Resources.view1;
             this.ptrView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.ptrView1.Location = new System.Drawing.Point(475, 258);
+            this.ptrView1.Location = new System.Drawing.Point(534, 322);
             this.ptrView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ptrView1.Name = "ptrView1";
-            this.ptrView1.Size = new System.Drawing.Size(36, 30);
+            this.ptrView1.Size = new System.Drawing.Size(40, 38);
             this.ptrView1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ptrView1.TabIndex = 19;
             this.ptrView1.TabStop = false;
@@ -165,9 +170,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("League Spartan SemiBold", 12F, System.Drawing.FontStyle.Bold);
             this.label4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label4.Location = new System.Drawing.Point(36, 228);
+            this.label4.Location = new System.Drawing.Point(40, 285);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(216, 30);
+            this.label4.Size = new System.Drawing.Size(257, 36);
             this.label4.TabIndex = 14;
             this.label4.Text = "Xác nhận mật khẩu mới";
             // 
@@ -176,10 +181,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("League Spartan SemiBold", 12F, System.Drawing.FontStyle.Bold);
             this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label3.Location = new System.Drawing.Point(36, 160);
+            this.label3.Location = new System.Drawing.Point(40, 200);
             this.label3.Margin = new System.Windows.Forms.Padding(0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(131, 30);
+            this.label3.Size = new System.Drawing.Size(155, 36);
             this.label3.TabIndex = 13;
             this.label3.Text = "Mật khẩu mới";
             // 
@@ -187,12 +192,12 @@
             // 
             this.txtXNMK.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtXNMK.Font = new System.Drawing.Font("League Spartan", 11F);
-            this.txtXNMK.Location = new System.Drawing.Point(41, 258);
+            this.txtXNMK.Location = new System.Drawing.Point(46, 322);
             this.txtXNMK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtXNMK.Multiline = true;
             this.txtXNMK.Name = "txtXNMK";
             this.txtXNMK.PasswordChar = '*';
-            this.txtXNMK.Size = new System.Drawing.Size(479, 30);
+            this.txtXNMK.Size = new System.Drawing.Size(539, 38);
             this.txtXNMK.TabIndex = 8;
             this.txtXNMK.TextChanged += new System.EventHandler(this.txtXNMK_TextChanged);
             // 
@@ -200,12 +205,12 @@
             // 
             this.txtMK.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtMK.Font = new System.Drawing.Font("League Spartan", 11F);
-            this.txtMK.Location = new System.Drawing.Point(41, 189);
+            this.txtMK.Location = new System.Drawing.Point(46, 236);
             this.txtMK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMK.Multiline = true;
             this.txtMK.Name = "txtMK";
             this.txtMK.PasswordChar = '*';
-            this.txtMK.Size = new System.Drawing.Size(479, 30);
+            this.txtMK.Size = new System.Drawing.Size(539, 38);
             this.txtMK.TabIndex = 7;
             this.txtMK.TextChanged += new System.EventHandler(this.txtMK_TextChanged);
             // 
@@ -216,13 +221,14 @@
             this.btnXacnhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
             this.btnXacnhan.ForeColor = System.Drawing.SystemColors.Control;
             this.btnXacnhan.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnXacnhan.Location = new System.Drawing.Point(41, 312);
+            this.btnXacnhan.Location = new System.Drawing.Point(46, 396);
             this.btnXacnhan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnXacnhan.Name = "btnXacnhan";
-            this.btnXacnhan.Size = new System.Drawing.Size(478, 47);
+            this.btnXacnhan.Size = new System.Drawing.Size(538, 59);
             this.btnXacnhan.TabIndex = 5;
             this.btnXacnhan.Text = "Xác nhận";
             this.btnXacnhan.UseVisualStyleBackColor = false;
+            this.btnXacnhan.Click += new System.EventHandler(this.btnXacnhan_Click);
             // 
             // Changepwd_label
             // 
@@ -230,24 +236,53 @@
             this.Changepwd_label.AutoSize = true;
             this.Changepwd_label.Font = new System.Drawing.Font("League Spartan", 23.8F, System.Drawing.FontStyle.Bold);
             this.Changepwd_label.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.Changepwd_label.Location = new System.Drawing.Point(93, 5);
+            this.Changepwd_label.Location = new System.Drawing.Point(105, 6);
             this.Changepwd_label.Margin = new System.Windows.Forms.Padding(0);
             this.Changepwd_label.Name = "Changepwd_label";
-            this.Changepwd_label.Size = new System.Drawing.Size(355, 60);
+            this.Changepwd_label.Size = new System.Drawing.Size(425, 71);
             this.Changepwd_label.TabIndex = 0;
             this.Changepwd_label.Text = "Thay đổi mật khẩu";
             this.Changepwd_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // ptrNotsame
+            // 
+            this.ptrNotsame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(227)))), ((int)(((byte)(243)))));
+            this.ptrNotsame.Image = global::Login.Properties.Resources.exclamation;
+            this.ptrNotsame.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.ptrNotsame.Location = new System.Drawing.Point(47, 365);
+            this.ptrNotsame.Name = "ptrNotsame";
+            this.ptrNotsame.Size = new System.Drawing.Size(23, 23);
+            this.ptrNotsame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptrNotsame.TabIndex = 24;
+            this.ptrNotsame.TabStop = false;
+            this.ptrNotsame.Visible = false;
+            // 
+            // lbNotsame
+            // 
+            this.lbNotsame.AutoSize = true;
+            this.lbNotsame.Font = new System.Drawing.Font("League Spartan SemiBold", 8F, System.Drawing.FontStyle.Bold);
+            this.lbNotsame.ForeColor = System.Drawing.Color.IndianRed;
+            this.lbNotsame.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lbNotsame.Location = new System.Drawing.Point(73, 365);
+            this.lbNotsame.Name = "lbNotsame";
+            this.lbNotsame.Size = new System.Drawing.Size(205, 23);
+            this.lbNotsame.TabIndex = 23;
+            this.lbNotsame.Text = "Mật khẩu không trùng khớp";
+            this.lbNotsame.Visible = false;
+            // 
             // Change_Password
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Login.Properties.Resources.image1;
-            this.ClientSize = new System.Drawing.Size(1687, 819);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
             this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Change_Password";
-            this.Text = "Chang_Password";
+            this.Text = "Change_Password";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Change_Password_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -256,6 +291,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.prtHide)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptrView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptrView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrNotsame)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -277,5 +313,7 @@
         private System.Windows.Forms.Label lbChangpwd;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox ptrHide1;
+        private System.Windows.Forms.PictureBox ptrNotsame;
+        private System.Windows.Forms.Label lbNotsame;
     }
 }
