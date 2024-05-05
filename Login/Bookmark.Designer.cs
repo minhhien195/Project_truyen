22e1a52cd129a1fa36d9c6a39d598c35e8d8faf6
Nguyễn Khắc Hậu <22520410@gm.uit.edu.vn>
2024-05-05 15:18:10 +0700

    edit bookmark function

--- Login/Bookmark.Designer.cs
+++ Login/Bookmark.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.labelTenTruyen = new System.Windows.Forms.Label();
-            this.labelChuongDangDoc = new System.Windows.Forms.Label();
             this.iconButtonDel = new FontAwesome.Sharp.IconButton();
+            this.labelChuongDangDoc = new System.Windows.Forms.Label();
+            this.labelTenTruyen = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -49,49 +49,9 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1056, 183);
+            this.panel1.Size = new System.Drawing.Size(1281, 183);
             this.panel1.TabIndex = 0;
             this.panel1.Visible = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = global::Login.Properties.Resources.Album_truyen;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(130, 177);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // panel2
-            // 
-            this.panel2.AutoSize = true;
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(3, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(130, 177);
-            this.panel2.TabIndex = 1;
-            // 
-            // labelTenTruyen
-            // 
-            this.labelTenTruyen.AutoSize = true;
-            this.labelTenTruyen.Font = new System.Drawing.Font("League Spartan", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelTenTruyen.Location = new System.Drawing.Point(139, 3);
-            this.labelTenTruyen.Name = "labelTenTruyen";
-            this.labelTenTruyen.Size = new System.Drawing.Size(695, 45);
-            this.labelTenTruyen.TabIndex = 2;
-            this.labelTenTruyen.Text = "Theo Giang Hồ Bắt Đầu, Liều Thành Võ Đạo Chân Quân";
-            // 
-            // labelChuongDangDoc
-            // 
-            this.labelChuongDangDoc.AutoSize = true;
-            this.labelChuongDangDoc.Font = new System.Drawing.Font("League Spartan", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelChuongDangDoc.Location = new System.Drawing.Point(139, 78);
-            this.labelChuongDangDoc.Name = "labelChuongDangDoc";
-            this.labelChuongDangDoc.Size = new System.Drawing.Size(154, 40);
-            this.labelChuongDangDoc.TabIndex = 3;
-            this.labelChuongDangDoc.Text = "Chương 100";
             // 
             // iconButtonDel
             // 
@@ -104,7 +64,7 @@
             this.iconButtonDel.IconChar = FontAwesome.Sharp.IconChar.TrashAlt;
             this.iconButtonDel.IconColor = System.Drawing.Color.White;
             this.iconButtonDel.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconButtonDel.Location = new System.Drawing.Point(838, 124);
+            this.iconButtonDel.Location = new System.Drawing.Point(1063, 124);
             this.iconButtonDel.Name = "iconButtonDel";
             this.iconButtonDel.Size = new System.Drawing.Size(215, 56);
             this.iconButtonDel.TabIndex = 5;
@@ -112,13 +72,53 @@
             this.iconButtonDel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.iconButtonDel.UseVisualStyleBackColor = false;
             // 
+            // labelChuongDangDoc
+            // 
+            this.labelChuongDangDoc.AutoSize = true;
+            this.labelChuongDangDoc.Font = new System.Drawing.Font("League Spartan", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelChuongDangDoc.Location = new System.Drawing.Point(139, 78);
+            this.labelChuongDangDoc.Name = "labelChuongDangDoc";
+            this.labelChuongDangDoc.Size = new System.Drawing.Size(158, 41);
+            this.labelChuongDangDoc.TabIndex = 3;
+            this.labelChuongDangDoc.Text = "Chương 100";
+            // 
+            // labelTenTruyen
+            // 
+            this.labelTenTruyen.AutoSize = true;
+            this.labelTenTruyen.Font = new System.Drawing.Font("League Spartan", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelTenTruyen.Location = new System.Drawing.Point(139, 3);
+            this.labelTenTruyen.Name = "labelTenTruyen";
+            this.labelTenTruyen.Size = new System.Drawing.Size(695, 45);
+            this.labelTenTruyen.TabIndex = 2;
+            this.labelTenTruyen.Text = "Theo Giang Hồ Bắt Đầu, Liều Thành Võ Đạo Chân Quân";
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(130, 177);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::Login.Properties.Resources.Album_truyen;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 177);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Bookmark
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(247)))), ((int)(((byte)(253)))));
-            this.ClientSize = new System.Drawing.Size(1056, 529);
+            this.ClientSize = new System.Drawing.Size(1281, 558);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Bookmark";
@@ -126,8 +126,8 @@
             this.Load += new System.EventHandler(this.Bookmark_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
