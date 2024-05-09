d62336ed62f831d80e43f4af825c381cf8664a0a
Nguyễn Khắc Hậu <22520410@gm.uit.edu.vn>
2024-05-09 22:24:42 +0700

    share album function

--- Login/AlbumTruyen.cs
+++ Login/AlbumTruyen.cs
@@ -19,17 +19,20 @@ using Firebase.Auth.Providers;
 using Firebase.Auth;
 using System.Windows.Documents;
 using System.IO;
-
+using FireSharp.Response;
+using System.Security.Cryptography;
+using Google.Cloud.Firestore;
 
 namespace Login
 {
     public partial class AlbumTruyen : Form
     {
+        UserCredential user;
 
-        public AlbumTruyen()
+        public AlbumTruyen(UserCredential user)
         {
             InitializeComponent();
-
+            this.user = user;
         }
 
         private void AlbumTruyen_Load(object sender, EventArgs e)
@@ -550,6 +553,227 @@ namespace Login
         private void lbTenanh1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private async void ibtnChiaseAlbum_Click(object sender, EventArgs e)
+        {
+            IFirebaseConfig Config = new FirebaseConfig
+            {
+                AuthSecret = "38QvLmnKMHlQtJ9yZzCqqWytxeXimwt06ZnFfSc2",
+                BasePath = "https://healtruyen-default-rtdb.asia-southeast1.firebasedatabase.app/"
+            };
+            IFirebaseClient client = new FireSharp.FirebaseClient(Config);
+            FirebaseResponse response = await client.GetAsync("Nguoi_dung/" + user.User.Uid + "/Album");
+            Dictionary<string, object> res = response.ResultAs<Dictionary<string, object>>();
+            if (res != null)
+            {
+                string shareId = Guid.NewGuid().ToString();
+
+                string shareLink = $"Nguoi_dung/{user.User.Uid}/Album";
+                string key = "12345678912345678912345678912345";
+
+                string encrypt_Share_Link = Encrypt(shareLink, key);
+
+                //SetResponse r = await client.SetAsync("Nguoi_dung/" + user.User.Uid + "/Album/LinkShare", shareLink);
+                Clipboard.SetText(encrypt_Share_Link);
+                MessageBox.Show("Link đã lưu vào clipboard");
+            }
+
+        }
+
+        static string Encrypt(string plainText, string key)
+        {
+            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+                aes.Mode = CipherMode.ECB; // Chế độ mã hóa ECB
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                byte[] encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+
+        static string Decrypt(string encryptedText, string key)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+                aes.Mode = CipherMode.ECB; // Chế độ mã hóa ECB
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+
+        private async void btnAccessAlbum_Click(object sender, EventArgs e)
+        {
+            string key = "12345678912345678912345678912345";
+            string sharelink = Decrypt(tbLinkShare.Text, key);
+            //config firestore
+            string project = "healtruyen";
+            FirestoreDb db = FirestoreDb.Create(project);
+            //config realtimedb
+            IFirebaseConfig Config = new FirebaseConfig
+            {
+                AuthSecret = "38QvLmnKMHlQtJ9yZzCqqWytxeXimwt06ZnFfSc2",
+                BasePath = "https://healtruyen-default-rtdb.asia-southeast1.firebasedatabase.app/"
+            };
+            IFirebaseClient client = new FireSharp.FirebaseClient(Config);
+
+            FirebaseResponse res = await client.GetAsync(sharelink);
+            Dictionary<string, object> result = res.ResultAs<Dictionary<string, object>>();
+
+            if (result != null)
+            {
+                foreach (var item in result.Keys)
+                {
+                    string idTruyen = item;
+                    Google.Cloud.Firestore.DocumentReference doc = db.Collection("Truyen").Document(idTruyen);
+                    DocumentSnapshot snapshot = await doc.GetSnapshotAsync();
+
+                    string imgBase64 = snapshot.GetValue<string>("Anh");
+                    string tenTruyen = snapshot.GetValue<string>("Ten");
+                    int tongChuong = snapshot.GetValue<int>("So_chuong");
+                    string tacGia = snapshot.GetValue<string>("Tac_gia");
+                    List<string> type = snapshot.GetValue<List<string>>("The_loai");
+                    string theLoai = type[0];
+                    int status = snapshot.GetValue<int>("Trang_thai");
+                    string trangThai = "";
+                    if (status == 0)
+                    {
+                        trangThai = "Dừng cập nhật";   
+                    } else if (status == 1)
+                    {
+                        trangThai = "Đang ra";
+                    } else if (status == 2)
+                    {
+                        trangThai = "Hoàn thành";
+                    }
+
+                    Panel panel = new Panel();
+                    panelAlbumTruyen.Controls.Add(panel);
+
+                    panel.Dock = DockStyle.Top;
+                    panel.BringToFront();
+                    panel.AutoScroll = true;
+                    panel.AutoSize = true;
+
+                    Panel panelPicture = new Panel();
+                    panel.Controls.Add(panelPicture);
+                    panelPicture.AutoSize = true;
+                    panelPicture.Location = new Point(19, 14);
+                    panelPicture.Width = 152;
+                    panelPicture.Height = 193;
+                    panelPicture.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+
+                    PictureBox pictureBox = new PictureBox();
+                    panelPicture.Controls.Add(pictureBox);
+                    pictureBox.Dock = DockStyle.Fill;
+
+                    byte[] imageBytes = Convert.FromBase64String(imgBase64);
+
+                    using (MemoryStream memoryStream = new MemoryStream(imageBytes))
+                    {
+                        Bitmap bitmap = new Bitmap(memoryStream);
+                        pictureBox.Image = bitmap;
+                        pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+
+                    Label lbTenTruyen = new Label();
+                    panel.Controls.Add(lbTenTruyen);
+                    lbTenTruyen.Font = new Font("League Spartan SemiBold", 12);
+                    lbTenTruyen.AutoSize = true;
+                    lbTenTruyen.Location = new Point(
+                        panelPicture.Location.X + panelPicture.Width + 10,
+                        panelPicture.Location.Y
+                    );
+                    lbTenTruyen.Text = tenTruyen;
+                    lbTenTruyen.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+
+                    IconButton ibtnTongChuong = new IconButton();
+                    panel.Controls.Add(ibtnTongChuong);
+                    ibtnTongChuong.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+                    ibtnTongChuong.Font = new Font("League Spartan", 9, FontStyle.Regular);
+                    ibtnTongChuong.Text = tongChuong.ToString() + " chương";
+                    ibtnTongChuong.IconChar = IconChar.LayerGroup;
+                    ibtnTongChuong.Width = 198;
+                    ibtnTongChuong.Height = 40;
+                    ibtnTongChuong.IconColor = Color.Black;
+                    ibtnTongChuong.IconSize = 32;
+                    ibtnTongChuong.ForeColor = Color.Black;
+                    ibtnTongChuong.FlatStyle = FlatStyle.Flat;
+                    ibtnTongChuong.FlatAppearance.BorderSize = 0;
+                    ibtnTongChuong.BackColor = Color.FromArgb(220, 247, 253);
+                    ibtnTongChuong.TextAlign = ContentAlignment.MiddleLeft;
+                    ibtnTongChuong.TextImageRelation = TextImageRelation.ImageBeforeText;
+                    ibtnTongChuong.Location = new Point(
+                        lbTenTruyen.Location.X,
+                        lbTenTruyen.Location.Y + lbTenTruyen.Height + 10
+                    );
+
+                    FlowLayoutPanel flowLayoutPanel = new FlowLayoutPanel();
+                    panel.Controls.Add(flowLayoutPanel);
+
+                    Button btnTacGia = new Button();
+                    btnTacGia.Text = tacGia.ToString();
+                    btnTacGia.Font = new Font("League Spartan", 9, FontStyle.Bold);
+                    btnTacGia.ForeColor = Color.Red;
+                    btnTacGia.FlatStyle = FlatStyle.Flat;
+                    btnTacGia.FlatAppearance.BorderSize = 4;
+                    btnTacGia.Width = 138;
+                    btnTacGia.Height = 45;
+                    btnTacGia.Margin = new Padding(3, 3, 10, 3);
+                    btnTacGia.Location = new Point(0, 0);
+
+                    Button btnTrangThai = new Button();
+                    btnTrangThai.Text = trangThai.ToString();
+                    btnTrangThai.Font = new Font("League Spartan", 9, FontStyle.Bold);
+                    btnTrangThai.ForeColor = Color.Green;
+                    btnTrangThai.FlatStyle = FlatStyle.Flat;
+                    btnTrangThai.FlatAppearance.BorderSize = 4;
+                    btnTrangThai.Width = 109;
+                    btnTrangThai.Height = 45;
+                    btnTrangThai.Margin = new Padding(3, 3, 10, 3);
+                    btnTrangThai.Location = new Point(btnTacGia.Location.X + btnTacGia.Width + 3, 0);
+
+                    Button btnTheLoai = new Button();
+                    btnTheLoai.Text = theLoai.ToString();
+                    btnTheLoai.Font = new Font("League Spartan", 9, FontStyle.Bold);
+                    btnTheLoai.ForeColor = Color.Blue;
+                    btnTheLoai.FlatStyle = FlatStyle.Flat;
+                    btnTheLoai.FlatAppearance.BorderSize = 4;
+                    btnTheLoai.Width = 147;
+                    btnTheLoai.Height = 45;
+                    btnTheLoai.Margin = new Padding(3, 3, 10, 3);
+                    btnTheLoai.Location = new Point(btnTrangThai.Location.X + btnTacGia.Width + 3, 0);
+
+
+                    flowLayoutPanel.FlowDirection = FlowDirection.LeftToRight;
+                    flowLayoutPanel.Location = new Point(
+                        lbTenTruyen.Location.X,
+                        panelPicture.Location.Y + panelPicture.Height - btnTacGia.Height - 3
+                    );
+                    flowLayoutPanel.AutoSize = true;
+                    flowLayoutPanel.Controls.Add(btnTacGia);
+                    flowLayoutPanel.Controls.Add(btnTrangThai);
+                    flowLayoutPanel.Controls.Add(btnTheLoai);
+                    flowLayoutPanel.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+
+                    MessageBox.Show(flowLayoutPanel.Location.Y.ToString());
+                }
+            }
         }
     }
 }
